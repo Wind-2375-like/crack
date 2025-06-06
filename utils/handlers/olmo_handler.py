b6c6fbacb71c575aee54a37ca09ef9a67e66b277
Wind-2375-like <yiyang.feng.2375@gmail.com>
2025-06-06 11:50:54 +0000

    fixed olmo and qwen bugs; added result pkls

--- utils/handlers/olmo_handler.py
+++ utils/handlers/olmo_handler.py
@@ -14,14 +14,18 @@ class OlmoHandler(BaseModelHandler):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     def format_messages(self, messages):
-        return self.tokenizer.apply_chat_template(
-            messages,
+        processed_messages = []
+        for role, content in messages:
+            processed_messages.append({"role": role, "content": content})
+        return processed_messages
+
+    def generate_response(self, formatted_input, **kwargs):
+        input_applied_with_chat_template = self.tokenizer.apply_chat_template(
+            formatted_input,
             tokenize=False,
             add_generation_prompt=True
         )
-
-    def generate_response(self, formatted_input, **kwargs):
-        model_inputs = self.tokenizer([formatted_input], return_tensors="pt").to(self.device)
+        model_inputs = self.tokenizer([input_applied_with_chat_template], return_tensors="pt").to(self.device)
         
         temperature = kwargs.get("temperature", 0)
         top_p = kwargs.get("top_p", 1)
@@ -29,18 +33,21 @@ class OlmoHandler(BaseModelHandler):
         max_tokens = kwargs.get("max_tokens", 512)
         
         with torch.no_grad():
-            generated_ids = self.model.generate(
-                **model_inputs,
-                max_new_tokens=max_tokens,
-                do_sample=True,
-                temperature=temperature,
-                top_p=top_p,
-                num_return_sequences=n,
-            )
-            # Remove the prompt from the generated text
-            generated_ids = [
-                output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-            ]
-            response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-            torch.cuda.empty_cache()
-            return response
+            responses = []
+            for _ in range(n):
+                generated_ids = self.model.generate(
+                    **model_inputs,
+                    max_new_tokens=max_tokens,
+                    do_sample=True,
+                    temperature=temperature,
+                    top_p=top_p,
+                    num_return_sequences=1,
+                )
+                # Remove the prompt from the generated text
+                generated_ids = [
+                    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+                ]
+                response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+                responses.extend(response)
+                torch.cuda.empty_cache()
+            return responses
